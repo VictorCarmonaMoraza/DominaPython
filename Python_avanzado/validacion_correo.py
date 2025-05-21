f8d6d7e091ae8d7074289a21179c0548f5815d4b
Victor Carmona <victorcarmona9035.com>
2025-05-21 17:03:20 +0200

    Validacion de un correo electronico

---
+++ Python_avanzado/validacion_correo.py
@@ -0,0 +1,12 @@
+import re
+
+
+def email_valido(email):
+    formato_valid = r"^([a-z0-9]+[-_.])*[a-z0-9]+@[a-z0-9]+(\.[a-z]{2,})+$"
+    if re.match(formato_valid, email, re.IGNORECASE):
+        return True
+    return False
+
+
+valido = email_valido("ana@email.com")
+print("Email valido:",valido)
