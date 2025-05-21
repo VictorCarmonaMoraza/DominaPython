fe96a638ae530c6bef6844a0e643d09eaa1adca0
Victor Carmona <victorcarmona9035.com>
2025-05-21 15:35:04 +0200

    Dar formtao a textos en python

---
+++ Python_intermedio/formato_texto.py
@@ -0,0 +1,20 @@
+numero =1
+texto_format = "Funcion format: {}".format(numero)
+print(texto_format)
+
+
+
+#No deviuleve el numero
+text_f_string = "F-string: {numero}"
+print(text_f_string)
+
+#Añadir comillas dentro del texto
+text_comillas ="Ana dijo \"Buenos dias\""
+print(text_comillas)
+
+#Salto de linea
+text_multilinea = "Linea 1\nLinea 2"
+print(text_multilinea)
+
+texto_tabular = "Nombre\tApellido\nAna\tPinto"
+print(texto_tabular)
